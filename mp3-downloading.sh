85e0843c4333c49666adc8f439d4bff5984ce677
Dimittrikov Kalahari <70070265+Dimittrikov@users.noreply.github.com>
2021-08-28 22:23:15 +0300

    Update mp3-downloading.sh

--- mp3-downloading.sh
+++ mp3-downloading.sh
@@ -3,6 +3,6 @@
 filename=$1
 while read line; do
 link=$(ytfzf -a -L $line)
-youtube-dl --extract-audio --audio-format mp3 $link
+youtube-dl --extract-audio --audio-format mp3 --no-playlist --playlist-items 1 $link
 
 done < $filename
